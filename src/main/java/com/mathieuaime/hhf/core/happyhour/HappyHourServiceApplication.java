12b0e7cb669627aabe5db53af55317e880312d6e
Mathieu <aime.mathieu1@gmail.com>
2020-04-08 18:25:34 +0200

    Setup project

---
+++ src/main/java/com/mathieuaime/hhf/core/happyhour/HappyHourServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mathieuaime.hhf.happyhour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHourServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HappyHourServiceApplication.class, args);
+  }
+}
